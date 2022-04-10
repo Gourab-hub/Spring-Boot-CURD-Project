40ce432a1dbb0095467ee3ec963f67da66c76e55
Gourab Banik <58955646+Gourab-hub@users.noreply.github.com>
2022-04-10 23:10:38 +0530

    8. Exception Handling

--- demo/src/main/java/com/example/demo/services/CourseServices.java
+++ demo/src/main/java/com/example/demo/services/CourseServices.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.models.Course;
 
@@ -9,55 +10,52 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @Service
 public class CourseServices {
 
-    private List<Course> allcourse= new ArrayList<>();
+    private List<Course> allcourse = new ArrayList<>();
 
     public CourseServices() {
-        allcourse.add(new Course(1,"React.js","Gourab Banik"));
-        allcourse.add(new Course(2,"Angular.js","Anju Bhuiya"));
-        allcourse.add(new Course(3,"Spring Boot","Tiyasha Dey"));
+        allcourse.add(new Course(1, "React.js", "Gourab Banik"));
+        allcourse.add(new Course(2, "Angular.js", "Anju Bhuiya"));
+        allcourse.add(new Course(3, "Spring Boot", "Tiyasha Dey"));
     }
-
 
     public List<Course> allCourse() {
         return allcourse;
     }
 
-    public List<Course> postCourse(@RequestBody Course course) {
+    public List<Course> postCourse(Course course) {
         allcourse.add(course);
         return allcourse;
     }
 
-    public Course singleCourse(@PathVariable("cid") Integer courseId) {
+    public Optional<Course> singleCourse(Integer courseId) throws Exception {
+        Optional<Course> singleCourse = allcourse.stream()
+                .filter(course -> courseId.equals(course.getCourseId()))
+                .findAny();
 
-        Course singleCourse= allcourse.stream()
-                             .filter(course->courseId.equals(course.getCourseId()))
-                             .findAny()
-                             .orElse(new Course(404,"not found ","Not found"));
+        if (!singleCourse.isPresent()) {
+            throw new Exception("Course is not Present");
+        }
 
         return singleCourse;
     }
 
-    public List<Course> updateCourse(@PathVariable("cid") Integer courseId,@RequestBody Course updatedCourse) {
+    public List<Course> updateCourse(Integer courseId, @RequestBody Course updatedCourse) {
 
-        Course singleCourse= allcourse.stream()
-                             .filter(course->courseId.equals(course.getCourseId()))
-                             .findAny()
-                             .orElse(new Course(404,"not found ","Not found"));
-
+        Course singleCourse = allcourse.stream()
+                .filter(course -> courseId.equals(course.getCourseId()))
+                .findAny()
+                .orElse(new Course(404, "not found ", "Not found"));
 
         allcourse.set(allcourse.indexOf(singleCourse), updatedCourse);
         return allcourse;
     }
 
-
-    public List<Course> deleteCourse(@PathVariable("cid") Integer courseId) {
-        allcourse.removeIf(course->course.getCourseId().equals(courseId));
+    public List<Course> deleteCourse(Integer courseId) {
+        allcourse.removeIf(course -> course.getCourseId().equals(courseId));
         return allcourse;
     }
-
 
 }
